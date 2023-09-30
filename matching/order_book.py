02fd061b2b1797c36a91e45179d0cd12f18ea445
mutalisk999 <81849682@qq.com>
2023-09-30 21:50:51 +0800

    update

--- matching/order_book.py
+++ matching/order_book.py
@@ -1,8 +1,11 @@
 from decimal import Decimal
-from typing import List
+from typing import List, Dict
 
+from models.models import Product
 from models.types import Side, OrderType, TimeInForceType
 from utils.window import Window
+
+ORDER_ID_WINDOW_CAP = 10000
 
 
 class BookOrder(object):
@@ -19,9 +22,24 @@ class BookOrder(object):
 
 
 class OrderBookSnapshot(object):
-    def __init__(self, product_id, orders, trade_seq, log_seq, order_id_window):
+    def __init__(self, product_id: str, orders: List, trade_seq: int, log_seq: int, order_id_window: Window):
         self.product_id: str = product_id
         self.orders: List[BookOrder] = orders
         self.trade_seq: int = trade_seq
         self.log_seq: int = log_seq
         self.order_id_window: Window = order_id_window
+
+
+class Depth(object):
+    def __init__(self):
+        self.orders: Dict[int, BookOrder] = dict()
+        self.queue = None
+
+
+class OrderBook(object):
+    def __init__(self, product: Product, trade_seq: int, log_seq: int):
+        self.product: Product = product
+        self.depths: Dict[Side, Depth] = dict()
+        self.trade_seq = trade_seq
+        self.log_seq = log_seq
+        self.order_id_window = Window(0, ORDER_ID_WINDOW_CAP)
